52634341549ebabf303cc093b60bcca1720b8cde
anshita20dubey <anshita20dubey@gmail.com>
2024-06-27 18:37:27 +0530

    Development/Production Setuo

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,10 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const config = require("config");
 
-mongoose.connect("mongodb://127.0.0.1:27017/scatch").then(() => {
-    console.log("connected to db");
-})
-.catch((err) => {
-    console.log(err);
-});
+const dbgr = require("debug")("development:mongoose");
+
+mongoose
+  .connect(`${config.get("MONGODB_URI")}/scatch`)
+  .then(function () {
+    dbgr("connected");
+  })
+  .catch(function (err) {
+    dbgr(err);
+  });
 
 module.exports = mongoose.connection;
